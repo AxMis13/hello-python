ca1c9b86d9a2dd5c1fd68f521bcd4b1cf33c3c41
Alexa Padilla <alexa_mishima@proton.me>
2025-09-18 12:44:35 -0600

    Operadores aritméticos, comparativos y lógicos

---
+++ 02_operators.py
@@ -0,0 +1,38 @@
+# Opradores aritméticos
+
+print(13 + 7)
+print(13 - 7)
+print(13 * 7)
+print(13 / 7)
+print(13 % 7) # operación módulo, calcúla el resto de la división
+
+print(13 // 7) # floor division, aproxíma el resultado a un entero
+print(13 ** 7) # calcúla el exponente
+
+print("Hola " + "Python") # concatena cadenas
+print("Hola" * 3)
+print("Hola" + str(5))
+
+# Operadores comparativos
+print("Operadores comparativos con números:")
+print(13 > 7)
+print(13 < 7)
+print(13 >= 7)
+print(13 <= 7)
+print(13 == 7)
+print(13 != 7)
+
+print("Operadores comparativos con cadenas:")
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("aaaa" >= "abaa") # ordenación alfabética por ASCII
+print("Hola" <= "Python")
+print("Hola" == "Python")
+print("Hola" != "Python")
+
+# Operadores lógicos
+print("Operadores lógicos:")
+print(13 > 7 and "Hola" > "Python")
+print(13 > 7 or "Hola" > "Python")
+print(not(13 > 7 and "Hola" > "Python"))
+
